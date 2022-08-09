b26e125d0ee54f22485427a537b0d0e8b2826dcd
Kevin <kevin.rodriguez678@gmail.com>
2022-08-09 13:19:35 -0400

    create card model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+  name = models.CharField(max_length=100)
+  nation = models.CharField(max_length=100)
+  position = models.CharField(max_length=100)
+  team = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name

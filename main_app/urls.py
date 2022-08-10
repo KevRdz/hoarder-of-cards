db24d3817c89eda72fbaaa4cb16be63cd53e8d46
Kevin <kevin.rodriguez678@gmail.com>
2022-08-10 14:23:19 -0400

    add signup page

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   path('cards/<int:card_id>/', views.cards_detail, name='cards_detail'),
   path('cards/create/', views.CardCreate.as_view(), name='cards_create'),
   path('cards/<int:pk>/delete/', views.CardDelete.as_view(), name='cards_delete'),
-  path('cards/<int:pk>/update/', views.CardUpdate.as_view(), name='cards_update')
+  path('cards/<int:pk>/update/', views.CardUpdate.as_view(), name='cards_update'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Card
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 class Home(LoginView):
@@ -14,19 +16,40 @@ def cards_index(request):
   cards = Card.objects.all()
   return render(request, 'cards/index.html', {'cards': cards})
 
+class CardCreate(CreateView):
+  model= Card
+  fields='__all__'
+  success_url='/cards/'
+
+  def from_valid(self, form):
+    form.instance.user= self.request.user
+    return super().form_valid(form)
+
 def cards_detail(request, card_id):
   card = Card.objects.get(id=card_id)
   return render(request, 'cards/detail.html', {'card': card})
 
-class CardCreate(CreateView):
-  model=Card
-  fields='__all__'
-  success_url='/cards/'
 
 class CardDelete(DeleteView):
   model = Card
   success_url = '/cards/'
 
 class CardUpdate(UpdateView):
-  model=Card
+  model= Card
   fields = ['name', 'nation', 'position', 'team']
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cards_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message':
+  error_message}
+  return render(request, 'signup.html', context)
